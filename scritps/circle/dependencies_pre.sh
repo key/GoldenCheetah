df91e05313ac2215c34ef4065d2181a67e7ba1c1
Mitsukuni Sato <mitsukuni.sato@gmail.com>
2015-03-01 16:34:27 +0900

    build without srmio & d2xx

--- scritps/circle/dependencies_pre.sh
+++ scritps/circle/dependencies_pre.sh
@@ -5,5 +5,7 @@ sudo aptitude update
 sudo aptitude install -y libqt4-dev qt4-dev-tools git
 
 # copy build configurations
-cp src/gcconfig.pri.in src/gcconfig.pri
+# FIXME anyone change paths in gcconfig.pri.in
+# cp src/gcconfig.pri.in src/gcconfig.pri
+cp scripts/circle/gcconfig.prig src/gcconfig.pri
 cp qwt/qwtconfig.pri.in qwt/qwtconfig.pri
