911cf93f2a438d1cf212653d5f6e65e587a1f826
Mitsukuni Sato <mitsukuni.sato@gmail.com>
2015-03-01 16:25:50 +0900

    update script

--- scritps/circle/dependencies_pre.sh
+++ scritps/circle/dependencies_pre.sh
@@ -2,8 +2,7 @@
 
 # install depended packages
 sudo aptitude update
-sudo aptitude install -y libqt4-dev qt4-dev-tools
-sudo aptitude install git
+sudo aptitude install -y libqt4-dev qt4-dev-tools git
 
 # copy build configurations
 cp src/gcconfig.pri.in src/gcconfig.pri
